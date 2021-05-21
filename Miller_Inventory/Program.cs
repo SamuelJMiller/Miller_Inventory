06745b342cc37c16d1ed7cb26f93523c573de274
samuel miller <samuel.miller@student.centralia.edu>
2021-05-20 20:44:05 -0700

    Show manifest

--- Miller_Inventory/Program.cs
+++ Miller_Inventory/Program.cs
@@ -7,7 +7,7 @@ namespace Miller_Inventory
 {
     class Program
     {
-        static void Main(string[] args)
+        static void show_menu()
         {
             Console.WriteLine("Choose from the following options:" +
                 "\n1. Add a Bicycle to the shipment" +
@@ -16,6 +16,80 @@ namespace Miller_Inventory
                 "\n4. Add Crackers to the shipment" +
                 "\n5. List Shipment Items" +
                 "\n6. Compute Shipping Charges");
+        }
+
+        static void cart_confirmation(string product)
+        {
+            Console.WriteLine("1 " + product + " has been added" +
+                "\nPress any key to return to the menu");
+            Console.ReadLine();
+        }
+
+        static void display_manifest_item(string product, int count)
+        {
+            if (count == 1)
+                Console.WriteLine(" " + product);
+            else
+                Console.WriteLine(" " + product + "s");
+        }
+
+        static void Main(string[] args)
+        {
+            // Shopping cart:
+            Shipper cart = new Shipper();
+
+            while (true)
+            {
+                // Show menu for first time:
+                show_menu();
+
+                // Get number choice with assumption that data will be acceptable:
+                int user_choice = Int32.Parse(Console.ReadLine());
+
+                switch (user_choice)
+                {
+                    case 1:
+                        cart.Add(new Bicycle());
+                        cart_confirmation("Bicycle");
+                        break;
+                    case 2:
+                        cart.Add(new LawnMower());
+                        cart_confirmation("Lawn Mower");
+                        break;
+                    case 3:
+                        cart.Add(new BaseballGlove());
+                        cart_confirmation("Baseball Glove");
+                        break;
+                    case 4:
+                        cart.Add(new Crackers());
+                        cart_confirmation("Crackers");
+                        break;
+                    case 5:
+                        int[] manifest = cart.get_shipment_manifest();
+
+                        Console.Write("Shipment manifest:" +
+                            "\n" + manifest[0] + " ");
+                        display_manifest_item("Bicycle", manifest[0]);
+
+                        Console.Write(manifest[1] + " ");
+                        display_manifest_item("Lawn Mower", manifest[1]);
+
+                        Console.Write(manifest[2] + " ");
+                        display_manifest_item("Baseball Glove", manifest[2]);
+
+                        Console.Write(manifest[3] + " ");
+                        Console.WriteLine("Crackers");
+
+                        Console.WriteLine("Press any key to continue");
+                        Console.Read();
+
+                        break;
+                    case 6:
+                        // Show message and exit program:
+                        Console.WriteLine("Total shipping cost for this order is $" + cart.get_total_cost());
+                        return;
+                }
+            }
         }
     }
 }
--- Miller_Inventory/Shipper.cs
+++ Miller_Inventory/Shipper.cs
@@ -8,10 +8,54 @@ namespace Miller_Inventory
 {
     class Shipper
     {
-        //  Adds an item to the "cart"
+        // Variable-length list for cart items:
+        List<IShippable> cart = new List<IShippable>();
+
+        //  Adds an item to the cart:
         public void Add(IShippable product)
         {
+            cart.Add(product);
+        }
 
+        public decimal get_total_cost()
+        {
+            decimal total = 0;
+
+            for ( int i = 0; i < cart.Count; ++i )
+            {
+                
+                // Add the appropriate shipping cost to total:
+                total += cart[i].ShipCost;
+            }
+
+            return total;
+        }
+
+        public int[] get_shipment_manifest()
+        {
+            int[] manifest = { 0, 0, 0, 0 };
+
+            // Add to correct array entries based on type of goods:
+            for ( int i = 0; i < cart.Count; ++i )
+            {
+                switch (cart[i].Product)
+                {
+                    case "Bicycle":
+                        manifest[0] += 1;
+                        break;
+                    case "Lawn Mower":
+                        manifest[1] += 1;
+                        break;
+                    case "Baseball Glove":
+                        manifest[2] += 1;
+                        break;
+                    case "Crackers":
+                        manifest[3] += 1;
+                        break;
+                }
+            }
+
+            return manifest;
         }
     }
 }
