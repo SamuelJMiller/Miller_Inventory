f99124ba96c3ad6857c46db1973e279b61a2bc7b
samuel miller <samuel.miller@student.centralia.edu>
2021-05-20 18:54:33 -0700

    Initial message

--- Miller_Inventory/Program.cs
+++ Miller_Inventory/Program.cs
@@ -9,7 +9,13 @@ namespace Miller_Inventory
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Choose from the following options:" +
+                "\n1. Add a Bicycle to the shipment" +
+                "\n2. Add a Lawn Mower to the Shipment" +
+                "\n3. Add a Baseball Glove to the shipment" +
+                "\n4. Add Crackers to the shipment" +
+                "\n5. List Shipment Items" +
+                "\n6. Compute Shipping Charges");
         }
     }
 }
